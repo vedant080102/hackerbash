b7f24eb6c0b15a53b862ee993947cd855c6bdc1f
Omkar <omksawant18@gmail.com>
2021-02-28 03:03:45 +0530

    final backend completed!!

--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from orders.views import bookItems
+from orders.views import bookItems,retBook
 
 # Create your views here.
 
@@ -14,8 +14,12 @@ def book(request):
     return render(request,'books.html',{"bk":bk})
 
 def iBook(request,name):
-    books=bookItems()
-    print(type(books))
+    books=retBook()
     for j in books:
-        print(j)
-    return render(request,'iBook.html',{"bkn":bkn})
+        if(j['Bname']==name):
+            bkn=name
+            bka=j['Author']
+            bkg=j['Genre']
+            bkp=j['Price']
+            bks=j['Summary']
+    return render(request,'iBook.html',{"bkn":bkn,"bka":bka,"bkg":bkg,"bkp":bkp,"bks":bks})
